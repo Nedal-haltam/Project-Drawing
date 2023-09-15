7e6e56a3235aa1e73f4eb1467abf48ca2afb2097
Nedal Abu Haltam <133881380+Nedal-haltam@users.noreply.github.com>
2023-09-15 16:23:03 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Project_Drawing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnlabels = new System.Windows.Forms.Button();
+            this.cbbrushsize = new System.Windows.Forms.ToolStripComboBox();
+            this.brushSizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eraseAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eraserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.brushToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btntextbox = new System.Windows.Forms.Button();
+            this.colorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colord = new System.Windows.Forms.ColorDialog();
+            this.linkALinkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 992);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 23);
+            this.button1.TabIndex = 18;
+            this.button1.TabStop = false;
+            this.button1.Text = "no draw mode";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // btnlabels
+            // 
+            this.btnlabels.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnlabels.Location = new System.Drawing.Point(235, 992);
+            this.btnlabels.Name = "btnlabels";
+            this.btnlabels.Size = new System.Drawing.Size(75, 23);
+            this.btnlabels.TabIndex = 12;
+            this.btnlabels.TabStop = false;
+            this.btnlabels.Text = "labels";
+            this.btnlabels.UseVisualStyleBackColor = true;
+            this.btnlabels.Visible = false;
+            // 
+            // cbbrushsize
+            // 
+            this.cbbrushsize.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.cbbrushsize.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+            this.cbbrushsize.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbrushsize.FlatStyle = System.Windows.Forms.FlatStyle.Standard;
+            this.cbbrushsize.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40"});
+            this.cbbrushsize.Name = "cbbrushsize";
+            this.cbbrushsize.Size = new System.Drawing.Size(121, 28);
+            this.cbbrushsize.SelectedIndexChanged += new System.EventHandler(this.cbbrushsize_SelectedIndexChanged);
+            // 
+            // brushSizeToolStripMenuItem
+            // 
+            this.brushSizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cbbrushsize});
+            this.brushSizeToolStripMenuItem.Name = "brushSizeToolStripMenuItem";
+            this.brushSizeToolStripMenuItem.Size = new System.Drawing.Size(88, 24);
+            this.brushSizeToolStripMenuItem.Text = "Brush size";
+            // 
+            // eraseAllToolStripMenuItem
+            // 
+            this.eraseAllToolStripMenuItem.Name = "eraseAllToolStripMenuItem";
+            this.eraseAllToolStripMenuItem.Size = new System.Drawing.Size(78, 24);
+            this.eraseAllToolStripMenuItem.Text = "Erase all";
+            this.eraseAllToolStripMenuItem.Click += new System.EventHandler(this.btnerase_Click);
+            // 
+            // eraserToolStripMenuItem
+            // 
+            this.eraserToolStripMenuItem.Name = "eraserToolStripMenuItem";
+            this.eraserToolStripMenuItem.Size = new System.Drawing.Size(63, 24);
+            this.eraserToolStripMenuItem.Text = "Eraser";
+            this.eraserToolStripMenuItem.Click += new System.EventHandler(this.btneraser_Click);
+            // 
+            // brushToolStripMenuItem
+            // 
+            this.brushToolStripMenuItem.Name = "brushToolStripMenuItem";
+            this.brushToolStripMenuItem.Size = new System.Drawing.Size(59, 24);
+            this.brushToolStripMenuItem.Text = "Brush";
+            this.brushToolStripMenuItem.Click += new System.EventHandler(this.btndraw_Click);
+            // 
+            // btntextbox
+            // 
+            this.btntextbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btntextbox.Location = new System.Drawing.Point(138, 992);
+            this.btntextbox.Name = "btntextbox";
+            this.btntextbox.Size = new System.Drawing.Size(75, 23);
+            this.btntextbox.TabIndex = 13;
+            this.btntextbox.TabStop = false;
+            this.btntextbox.Text = "text box";
+            this.btntextbox.UseVisualStyleBackColor = true;
+            this.btntextbox.Visible = false;
+            // 
+            // colorsToolStripMenuItem
+            // 
+            this.colorsToolStripMenuItem.Name = "colorsToolStripMenuItem";
+            this.colorsToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.colorsToolStripMenuItem.Text = "Colors";
+            this.colorsToolStripMenuItem.Click += new System.EventHandler(this.btncolor_Click);
+            // 
+            // colord
+            // 
+            this.colord.AnyColor = true;
+            this.colord.FullOpen = true;
+            // 
+            // linkALinkToolStripMenuItem
+            // 
+            this.linkALinkToolStripMenuItem.Name = "linkALinkToolStripMenuItem";
+            this.linkALinkToolStripMenuItem.Size = new System.Drawing.Size(140, 24);
+            this.linkALinkToolStripMenuItem.Text = "link a link";
+            // 
+            // deleteToolStripMenuItem1
+            // 
+            this.deleteToolStripMenuItem1.Name = "deleteToolStripMenuItem1";
+            this.deleteToolStripMenuItem1.Size = new System.Drawing.Size(140, 24);
+            this.deleteToolStripMenuItem1.Text = "delete";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem1,
+            this.linkALinkToolStripMenuItem});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(141, 52);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(120, 24);
+            this.deleteToolStripMenuItem.Text = "delete";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(121, 28);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btntextbox);
+            this.panel1.Controls.Add(this.btnlabels);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1924, 1027);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.colorsToolStripMenuItem,
+            this.brushToolStripMenuItem,
+            this.eraserToolStripMenuItem,
+            this.eraseAllToolStripMenuItem,
+            this.brushSizeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1924, 28);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip2.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnlabels;
+        private System.Windows.Forms.ToolStripComboBox cbbrushsize;
+        private System.Windows.Forms.ToolStripMenuItem brushSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eraseAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eraserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem brushToolStripMenuItem;
+        private System.Windows.Forms.Button btntextbox;
+        private System.Windows.Forms.ToolStripMenuItem colorsToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colord;
+        private System.Windows.Forms.ToolStripMenuItem linkALinkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
